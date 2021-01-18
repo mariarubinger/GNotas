3bb7a2705c4ca7ae2cae3c96dd12666aa4648c0b
Maria Rubinger <rubinger.maria@gmail.com>
2021-01-18 18:28:40 +0000

    Criar select de categorias no formulário

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
   constructor(){
     super();
 
+    //está guardando todos os dados da aplicação
+    //ele passa essas informaçoes pros filhos FormularioCadastro, ListaDeCategorias, ListaDeNotas
     this.state = {
       notas:[],
       categorias:[],
@@ -46,14 +48,19 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <FormularioCadastro
+          categorias={this.state.categorias}
+          criarNota={this.criarNota.bind(this)}
+        />
         <main className="conteudo-principal">
           <ListaDeCategorias
             adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}/>
+            categorias={this.state.categorias}
+          />
           <ListaDeNotas
             apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}/>
+            notas={this.state.notas}
+          />
 
         </main>
       </section>
