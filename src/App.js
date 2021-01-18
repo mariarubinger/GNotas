90eed12a7a3ba413fb16c285e57608b53d7b39ae
Maria Rubinger <rubinger.maria@gmail.com>
2021-01-18 13:33:25 +0000

    Criar formulario de cadastro de notas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
+import "./assets/App.css";
+import "./assets/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  //criarNota é uma propriedade props do FormularioCadastro
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota] 
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+
+   // console.log(this.notas.length)
+   //console.log(`uma nova nota foi criada` + titulo + " " + texto)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  } 
 }
+
+//o que tem abaixo do return é uma arvore de renderização
+//tem que encapsular os dois elementos que são irmãos, que são o form e o ul em uma section ou div 
 
 export default App;
