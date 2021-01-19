5fb1b69af23371cdb6bd073d6059ae510953b420
Maria Rubinger <rubinger.maria@gmail.com>
2021-01-19 12:17:49 +0000

    Criar fonte de dados

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 
 class App extends Component {
-  
   constructor(){
     super();
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+  }
 
     //está guardando todos os dados da aplicação
     //ele passa essas informaçoes pros filhos FormularioCadastro, ListaDeCategorias, ListaDeNotas
-    this.state = {
+ /*    this.state = {
       notas:[],
-      categorias:[],
+      categorias:["Games", "Música"],
     };
   }
-
+ */
   //criarNota é uma propriedade props do FormularioCadastro
-  criarNota(titulo, texto, categoria){
+/*   criarNota(titulo, texto, categoria){
     const novaNota = {titulo, texto, categoria};
     const novoArrayNotas = [...this.state.notas,novaNota] 
     const novoEstado = {
@@ -30,42 +34,42 @@ class App extends Component {
    // console.log(this.notas.length)
    //console.log(`uma nova nota foi criada` + titulo + " " + texto)
   }
-
-  adicionarCategoria(nomeCategoria){
+ */
+ /*  adicionarCategoria(nomeCategoria){
     const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
     const novoEstado = {...this.state, categorias:novoArrayCategorias};
     this.setState(novoEstado)
-
   }
-
-  deletarNota(index){
+ */
+/*   deletarNota(index){
     let arrayNotas = this.state.notas;
     arrayNotas.splice(index,1); //deletar itens
     this.setState({notas:arrayNotas})
   }
+ */
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro
-          categorias={this.state.categorias}
-          criarNota={this.criarNota.bind(this)}
+          categorias={this.categorias.categorias}
+          criarNota={this.notas.criarNota}
         />
         <main className="conteudo-principal">
           <ListaDeCategorias
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria}
+            categorias={this.categorias.categorias}
           />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}
+            apagarNota={this.notas.apagarNota}
+            notas={this.notas.notas}
           />
-
         </main>
       </section>
     );
-  } 
+  }
 }
+
 
 //o que tem abaixo do return é uma arvore de renderização
 //tem que encapsular os dois elementos que são irmãos, que são o form e o ul em uma section ou div 
